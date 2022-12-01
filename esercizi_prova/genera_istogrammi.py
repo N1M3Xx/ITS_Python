53d7adecedd33b1248d13bdcd54a4f6463eb24d7
N1M3Xx <119055821+N1M3Xx@users.noreply.github.com>
2022-12-01 11:33:17 +0100

    Update genera_istogrammi.py

--- esercizi_prova/genera_istogrammi.py
+++ esercizi_prova/genera_istogrammi.py
@@ -1,9 +1,26 @@
 def istogramma(n):
     print("\n\n")
+    m=n[0]
+    for x in range(len(n)-1):
+        if((m>n[x+1]) and ((n[x+1]<0) or (m<0))):
+            m=n[x+1]
+        elif(m<=n[x+1]):
+            continue
+        else:
+            m=0
     for x in range(len(n)):
         a=""
-        for y in range(n[x]):
-            a=a+"*"
+        if(n[x]>=m):
+            if(n[x]>0):
+                for z in range((m*-1)):
+                    a=a+" "
+                for y in range(n[x]):
+                    a=a+"*"
+            else:
+                for z in range(n[x]+(m*-1)):
+                    a=a+" "
+                for y in range(n[x]*-1):
+                    a=a+"*"
         print(a)
         
 c=0
@@ -12,16 +29,14 @@ l=[]
 
 while(c==0):
     ct+=1
-    print("\n\nInserisci il ", ct,"° numero positivo: ")
+    print("\n\nInserisci il ", ct,"° numero: ")
     n=input("")
-    if(n.isnumeric()==True):
+    if((n.isnumeric()==True) or ((n[1:].isnumeric()) and n[0]=="-")):
         n=int(n)
-        if(n>0):
-            l.append(n)
-        elif(n<0):
-            print("\nErrore di inserimento\n")
-        else:
+        if(n==0):
             c=1
+        else:
+            l.append(n)
     else:
         print("\nErrore di inserimento\n")
         
