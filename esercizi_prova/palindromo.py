8abdda3451e73c1991d26d66b69567bff5ffc16f
N1M3Xx <119055821+N1M3Xx@users.noreply.github.com>
2022-12-02 14:57:08 +0100

    Update palindromo.py

--- esercizi_prova/palindromo.py
+++ esercizi_prova/palindromo.py
@@ -1,6 +1,6 @@
 def rendi_palindromo(a):
     for y in range(int(len(a)/2)):
-        for x in range(y, int(len(a)/2)):
+        for x in range(y, int(len(a)/2)-y):
             if(a[x]!=a[len(a)-1-x]):
                 if(a[x]>a[len(a)-1-x]):
                     n=a[x]-a[len(a)-1-x]
@@ -12,7 +12,7 @@ def rendi_palindromo(a):
                     a[len(a)-1-x]=a[x]
     return a
 
-a=[1, 5, 1, 5, 6]
+a=[1, 2, 3, 4, 5, 6, 7, 8, 9]
 palindromo=True
 
 print(a)
