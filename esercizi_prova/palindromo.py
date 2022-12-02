9e813233380bde3dd1044812afdcef3e71bb7740
N1M3Xx <119055821+N1M3Xx@users.noreply.github.com>
2022-12-02 14:50:04 +0100

    Create palindromo.py

---
+++ esercizi_prova/palindromo.py
@@ -0,0 +1,28 @@
+def rendi_palindromo(a):
+    for y in range(int(len(a)/2)):
+        for x in range(y, int(len(a)/2)):
+            if(a[x]!=a[len(a)-1-x]):
+                if(a[x]>a[len(a)-1-x]):
+                    n=a[x]-a[len(a)-1-x]
+                    a.insert(x+1, n)
+                    a[x]=a[len(a)-1-x]
+                else:
+                    n=a[len(a)-1-x]-a[x]
+                    a.insert(x+1, n)
+                    a[len(a)-1-x]=a[x]
+    return a
+
+a=[1, 5, 1, 5, 6]
+palindromo=True
+
+print(a)
+
+for x in range(int(len(a)/2)):
+    if(a[x]!=a[len(a)-1-x]):
+        palindromo=False
+        
+if(palindromo==True):
+    print("\nL'array è palindromo")
+else:
+    a=rendi_palindromo(a)
+    print("\nL'array convertito è ", a)
