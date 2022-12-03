da0e942d359dbf63a4773fccc4d66ef059b95ac5
N1M3Xx <119055821+N1M3Xx@users.noreply.github.com>
2022-12-03 16:19:28 +0100

    Update algoritmo_vita.py

--- esercizi_prova/algoritmo_vita.py
+++ esercizi_prova/algoritmo_vita.py
@@ -2,6 +2,7 @@ def primo():
     n_b=0
     anno=0
 
+    print("\nLa persona segue elementari e medie")
     while((anno<5) and (n_b<3)):
         print("\n", anno+1, "° anno delle superiori")
         r=input("\nSei stato bocciato? ")
@@ -9,26 +10,28 @@ def primo():
             n_b+=1
         else:
             anno+=1
-
-    print("\nInizia il Lavoro")
+    secondo()
     
 def secondo():
     n_p=0
 
+    print("\nInizia il Lavoro")
     for x in range(30):
         r=input("\nTi sei infortunato? ")
         if(r=="no"):
-            print("\nallora lavori e tasi")
-            r=input("\nPromozione? ")
-            if(r=="si"):
-                n_p+=1
+            n_p=terzo(n_p)
             if(n_p==3):
+                print("\nSei diventato capo dell'azienda")
                 break
         else:
             break
-    
+def terzo(n_p):
+    print("\nallora lavori e tasi")
+    r=input("\nPromozione? ")
+    if(r=="si"):
+        n_p+=1
+    return n_p
 
 print("Nasce la persona")
-print("\nLa persona segue elementari e medie")
 primo()
 print("\nPENSIONE!!!!!!!!!!!")
