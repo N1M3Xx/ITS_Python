15b2e061ce9c4603c4d1e1e7615548dbf065c0e0
N1M3Xx <119055821+N1M3Xx@users.noreply.github.com>
2022-12-06 17:58:27 +0100

    Create 20_48.py

---
+++ esercizi_prova/20_48.py
@@ -0,0 +1,103 @@
+import pygame
+import time
+import random
+ 
+pygame.init()
+ 
+white = (255, 255, 255)
+yellow = (255, 255, 100)
+black = (0, 0, 0)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (50, 0, 213)
+ 
+dis_width = 800
+dis_height = 800
+ 
+dis = pygame.display.set_mode((dis_width, dis_height))
+pygame.display.set_caption('2048')
+ 
+clock = pygame.time.Clock()
+ 
+block = 200
+transition_speed = 5
+ 
+font_style = pygame.font.SysFont("bahnschrift", 25)
+score_font = pygame.font.SysFont("comicsansms", 35)
+ 
+ 
+def Your_score(score):
+    value = score_font.render("Your Score: " + str(score), True, yellow)
+    dis.blit(value, [0, 0])
+ 
+ 
+def message(msg, color):
+    mesg = font_style.render(msg, True, color)
+    dis.blit(mesg, [dis_width / 6, dis_height / 3])
+ 
+ 
+def gameLoop():
+    game_over = False
+    game_close = False
+ 
+    x1 = 0
+    y1 = 0
+ 
+    x1_change = 0
+    y1_change = 0
+ 
+    snake_List = []
+    Length_of_snake = 1
+ 
+    foodx = round(random.randrange(0, dis_width - block) / 10.0) * 10.0
+    foody = round(random.randrange(0, dis_height - block) / 10.0) * 10.0
+ 
+    while not game_over:
+ 
+        while game_close == True:
+            dis.fill(black)
+            message("Hai Perso! Premi ESC per uscire o C per continuare", red)
+            Your_score(Length_of_snake - 1)
+            pygame.display.update()
+ 
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        game_over = True
+                        game_close = False
+                    if event.key == pygame.K_c:
+                        gameLoop()
+ 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                        game_over = True
+                        game_close = True
+                if event.key == pygame.K_LEFT:
+                    x1_change = -block
+                    y1_change = 0
+                elif event.key == pygame.K_RIGHT:
+                    x1_change = block
+                    y1_change = 0
+                elif event.key == pygame.K_UP:
+                    y1_change = -block
+                    x1_change = 0
+                elif event.key == pygame.K_DOWN:
+                    y1_change = block
+                    x1_change = 0
+
+        x1+=x1_change
+        y1+=y1_change
+
+        dis.fill(black)
+        pygame.draw.rect(dis, green, [x1, y1, block, block])
+        pygame.display.update()
+        clock.tick(transition_speed)
+ 
+    pygame.quit()
+    quit()
+ 
+ 
+gameLoop()
